c7011be5c5b5a6204b106187124220c36a42c49d
tseevag <shivagaihre40@gmail.com>
2019-12-20 06:37:07 +0545

    show online item in different line

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -11,11 +11,13 @@ const Join = () => {
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading"> Join </h1>
-                <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)}/></div>
-                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)}/></div>
-                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button  className="button mt-20" type="submit">Sign in</button>
-                </Link>
+                <form>
+                    <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)}/></div>
+                    <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)}/></div>
+                    <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                        <button  className="button mt-20" type="submit">Sign in</button>
+                    </Link>
+                </form>
             </div>
         </div>
         )
